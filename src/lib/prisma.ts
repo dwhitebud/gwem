5012fa5582bc13089fb9c5ebb1c9ff69e36438d4
dwhitebud <dwhitebud@gmail.com>
2024-12-09 21:54:45 -0700

    More prisma adjustments

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,13 +4,21 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  log: ['query'],
-  datasources: {
-    db: {
-      url: process.env.POSTGRES_PRISMA_URL
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ['query'],
+    datasources: {
+      db: {
+        url: process.env.POSTGRES_PRISMA_URL
+      }
     }
-  }
-})
+  })
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+if (process.env.NODE_ENV === 'production') {
+  prisma.$connect()
+}
